041f412a4be87137c1d8624e31a69c6ae315e286
Erik Huang <ekhu94@gmail.com>
2021-04-09 10:13:12 -0700

    all styling for signup and login done

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -5,8 +5,10 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import { ErrorMessage } from '@hookform/error-message';
 import * as yup from 'yup';
 
+import { Link } from 'react-router-dom';
 import { api } from '../services/api';
-import { Card, Form, Row, Button } from 'react-bootstrap';
+import { Container, Card, Form, Row, Button } from 'react-bootstrap';
+import backgroundImg from '../assets/pro-controller.jpg';
 import './Signup.css'
 
 const schema = yup.object().shape({
@@ -50,90 +52,97 @@ const Signup = ({ onSignup, routerProps, showAlert, renderAlert }) => {
     return (
         <>
             {showAlert && renderAlert()}
-            <div className="container pt-5">
-                <Row className="justify-content-center">
-                <Card className="pt-5 pb-4 col-8" style={{ borderRadius: '20px' }}>
-                <h1 className="mb-4 text-center" style={{letterSpacing: '0.5rem'}}>Kyotendo Sign Up</h1>
-                <Form onSubmit={handleSubmit(onFormSubmit)}>
+            <div className="auth-page-container" style={{backgroundImage: `url(${backgroundImg})`, paddingBottom: '90px'}}>
+                <Container>
                     <Row className="justify-content-center">
-                        <div className="col-10 col-sm-8 col-md-7 my-3">
-                            <input
-                                type="text"
-                                className="form-control"
-                                name="username"
-                                placeholder="Username"
-                                {...register("username")}
-                            />
-                            <ErrorMessage
-                                errors={errors}
-                                name="username"
-                                render={({ message }) => <p className="mt-2 text-danger">{message}</p>}
-                            />
-                        </div>
-                    </Row>
-                    <Row className="justify-content-center">
-                    <div className="col-10 col-sm-8 col-md-7 my-3">
-                            <input
-                                type="text"
-                                className="form-control"
-                                name="email"
-                                placeholder="Email Address"
-                                {...register("email")}
-                            />
-                            <ErrorMessage
-                                errors={errors}
-                                name="email"
-                                render={({ message }) => <p className="mt-2 text-danger">{message}</p>}
-                            />
-                        </div>
+                    <Card className="pt-5 pb-4 col-10 col-md-8" style={{ borderRadius: '20px' }}>
+                    <h1 className="mb-4 text-center" style={{letterSpacing: '0.5rem'}}>Kyotendo Sign Up</h1>
+                    <Form onSubmit={handleSubmit(onFormSubmit)}>
+                        <Row className="justify-content-center">
+                            <div className="col-10 col-md-7 my-3">
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    name="username"
+                                    placeholder="Username"
+                                    {...register("username")}
+                                />
+                                <ErrorMessage
+                                    errors={errors}
+                                    name="username"
+                                    render={({ message }) => <p className="mt-2 text-danger">{message}</p>}
+                                />
+                            </div>
                         </Row>
-                    <Row className="justify-content-center">
-                        <div className="col-10 col-sm-8 col-md-7 my-3">
-                            <input
-                                className="form-control"
-                                type="password"
-                                name="password"
-                                placeholder="Password"
-                                {...register("password")}
-                            />
-                            <ErrorMessage
-                                errors={errors}
-                                name="password"
-                                render={({ message }) => <p className="mt-2 text-danger">{message}</p>}
-                            />
-                        </div>
+                        <Row className="justify-content-center">
+                        <div className="col-10 col-md-7 my-3">
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    name="email"
+                                    placeholder="Email Address"
+                                    {...register("email")}
+                                />
+                                <ErrorMessage
+                                    errors={errors}
+                                    name="email"
+                                    render={({ message }) => <p className="mt-2 text-danger">{message}</p>}
+                                />
+                            </div>
+                            </Row>
+                        <Row className="justify-content-center">
+                            <div className="col-10 col-md-7 my-3">
+                                <input
+                                    className="form-control"
+                                    type="password"
+                                    name="password"
+                                    placeholder="Password"
+                                    {...register("password")}
+                                />
+                                <ErrorMessage
+                                    errors={errors}
+                                    name="password"
+                                    render={({ message }) => <p className="mt-2 text-danger">{message}</p>}
+                                />
+                            </div>
+                        </Row>
+                        <Row className="justify-content-center">
+                            <div className="col-10 col-md-7 my-3">
+                                <input
+                                    className="form-control"
+                                    type="password"
+                                    name="passwordConfirmation"
+                                    placeholder="Confirm Password"
+                                    {...register("passwordConfirmation")}
+                                />
+                                <ErrorMessage
+                                    errors={errors}
+                                    name="passwordConfirmation"
+                                    render={() => <p className="mt-2 text-danger">passwords must match</p>}
+                                />
+                            </div>
+                        </Row>
+                        <Row className="justify-content-center">
+                            <Button
+                                variant="primary"
+                                size="lg"
+                                block
+                                style={{ borderRadius: '18px' }}
+                                className="auth-btn col-8 col-sm-6 col-md-5 mt-4 mb-3"
+                                type="submit"
+                            >
+                                Create Account
+                            </Button>
+                        </Row>
+                        <Row className="justify-content-center">
+                            <Link to="/login" exact>
+                                <div style={{color: 'var(--blue-secondary)'}} className="text-center">Returning User?</div>                              
+                            </Link>
+                        </Row>
+                    </Form>
+                    </Card>
                     </Row>
-                    <Row className="justify-content-center">
-                        <div className="col-10 col-sm-8 col-md-7 my-3">
-                            <input
-                                className="form-control"
-                                type="password"
-                                name="passwordConfirmation"
-                                placeholder="Confirm Password"
-                                {...register("passwordConfirmation")}
-                            />
-                            <ErrorMessage
-                                errors={errors}
-                                name="passwordConfirmation"
-                                render={() => <p className="mt-2 text-danger">passwords must match</p>}
-                            />
-                        </div>
-                    </Row>
-                    <Row className="justify-content-center">
-                        <Button
-                            variant="dark"
-                            size="lg"
-                            block
-                            style={{ borderRadius: '8px' }}
-                            className="col-6 col-lg-4 mt-4 mb-3"
-                            type="submit"
-                        >
-                            Create <span className="d-none d-md-inline-flex">New Account</span>
-                        </Button>
-                    </Row>
-                </Form>
-                </Card>
-                </Row>
+                </Container>
             </div>
         </>
     );
