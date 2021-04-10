61fa94489ec06e16a4ad28d9b436ebe14f42a0b2
Erik Huang <ekhu94@gmail.com>
2021-04-10 16:52:29 -0700

    users cannot upvote their own posts

--- src/components/UpvoteButtons.js
+++ src/components/UpvoteButtons.js
@@ -1,34 +1,55 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
 import { Container, Row } from 'react-bootstrap';
 import upvoteImg from '../assets/icons/mario-upvote-icon.jpg';
 import downvoteImg from '../assets/icons/bowser-downvote-icon.jpg';
 import './UpvoteButtons.css';
 import { api } from '../services/api';
 
-const UpvoteButtons = ({ postId, postUpvotes }) => {
+const UpvoteButtons = ({ auth, postId, postUpvotes }) => {
+    const [ownPost, setOwnPost] = useState(false);
     const [upvoteCount, setUpvoteCount] = useState(postUpvotes)
     const [upvoted, setUpvoted] = useState(false);
     const [downvoted, setDownvoted] = useState(false);
 
+    useEffect(() => {
+        const validateUpvoteClick = async () => {
+            if (auth.user.id) {
+                const res = await api.rails.get(`/users/${auth.user.id}`);
+                const post = res.data.posts.find(p => p.id === postId);
+                setOwnPost(!!post);
+            }
+        }
+        validateUpvoteClick();
+
+        return () => {
+            setOwnPost(false);
+        }
+    }, []);
+
     const onUpvoteClick = async () => {
-        setDownvoted(false);
-        if (upvoteCount >= postUpvotes) setUpvoted(true);
-        if (!upvoted) {
-            setUpvoteCount(upvoteCount + 1);
-            await api.rails.patch(`/posts/${postId}`, {
-                upvotes: upvoteCount + 1
-            });
+        if (!ownPost) {
+            setDownvoted(false);
+            if (upvoteCount >= postUpvotes) setUpvoted(true);
+            if (!upvoted) {
+                setUpvoteCount(upvoteCount + 1);
+                await api.rails.patch(`/posts/${postId}`, {
+                    upvotes: upvoteCount + 1
+                });
+            }
         }
     }
 
     const onDownvoteClick = async () => {
-        setUpvoted(false);
-        if (upvoteCount <= postUpvotes) setDownvoted(true);
-        if (!downvoted) {
-            setUpvoteCount(upvoteCount - 1);
-            await api.rails.patch(`/posts/${postId}`, {
-                upvotes: upvoteCount - 1
-            });
+        if (!ownPost) {
+            setUpvoted(false);
+            if (upvoteCount <= postUpvotes) setDownvoted(true);
+            if (!downvoted) {
+                setUpvoteCount(upvoteCount - 1);
+                await api.rails.patch(`/posts/${postId}`, {
+                    upvotes: upvoteCount - 1
+                });
+            }
         }
     }
 
@@ -62,4 +83,8 @@ const UpvoteButtons = ({ postId, postUpvotes }) => {
     );
 };
 
-export default UpvoteButtons;
+const mapStateToProps = state => {
+    return { auth: state.auth };
+}
+
+export default connect(mapStateToProps)(UpvoteButtons);
