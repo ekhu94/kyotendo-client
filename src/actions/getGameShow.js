262494254fd94250a424919bf416e43f7e464fa9
Erik Huang <ekhu94@gmail.com>
2021-04-12 22:42:07 -0700

    refactor card to scale out horizontally on hover

--- src/actions/getGameShow.js
+++ src/actions/getGameShow.js
@@ -3,11 +3,12 @@ import { api } from '../services/api';
 
 export const getGameShow = slug => {
     return async (dispatch) => {
+        const query = slug.split('-').join(' ');
         const res = await api.rawg.get('/games', {
             params: {
                 platforms: 7,
                 page_size: 1,
-                search: slug
+                search: query
             }
         });
         dispatch({ type: GET_GAME_SHOW, payload: res.data.results[0] })
