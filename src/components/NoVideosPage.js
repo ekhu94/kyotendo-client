19acc937edce3809bb5b002abcf617681f7d99d0
Erik Huang <ekhu94@gmail.com>
2021-04-14 11:23:27 -0700

    create novideopage to render when youtube quota is up

--- src/components/NoVideosPage.js
+++ src/components/NoVideosPage.js
@@ -1,8 +1,35 @@
 import React from 'react';
+import { Container, Row, Card } from 'react-bootstrap';
 
-const NoVideosPage = () => {
+import noVidImg from '../assets/no-videos.jpg';
+import BackButton from './BackButton';
+
+const NoVideosPage = ({ gameSlug, gameShow }) => {
+
     return (
-        <div>No more videos!</div>
+        <Container>
+            <BackButton label="back to game page" url={`/games/${gameSlug}`} />
+            <Row className="justify-content-center mt-2">
+                <Card style={{borderRadius: '20px'}} className="col-10 col-md-12 p-0">
+                    <h1 className="youtube-header text-center py-4 mb-0">{gameShow.name} Videos</h1>
+                        <h4 className="mt-5 mb-1 text-center">Oh dear! It looks like you've hit your quota for today.</h4>
+                        <h4 className="mb-4 text-center">Please try again later!</h4>
+                        <img
+                            src={noVidImg}
+                            alt="noposts"
+                            style={{
+                                width: '25%',
+                                height: 'auto',
+                                margin: '3rem auto',
+                                textAlign: 'center',
+                                border: 'none',
+                                borderBottomLeftRadius: '20px',
+                                borderBottomRightRadius: '20px'
+                            }}
+                        />
+                </Card>
+            </Row>
+        </Container>
     );
 };
 
--- src/components/YoutubeVideoPlayer.js
+++ src/components/YoutubeVideoPlayer.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
+import action from '../actions';
 import { Container, Row, Card, Button } from 'react-bootstrap';
 
 import './YoutubeVideoPlayer.css';
@@ -13,34 +14,32 @@ import YoutubeVideoList from './YoutubeVideoList';
 //! custom hook
 import useVideos from '../hooks/useVideos';
 
-const YoutubeVideoPlayer = ({ gameSlug }) => {
+const YoutubeVideoPlayer = ({ gameSlug, gameShow, getGameShow, resetGameShow }) => {
     const [selectedVideo, setSelectedVideo] = useState(null);
     const [quotaDone, setQuotaDone] = useState(false);
     const {videos, search} = useVideos(`${gameSlug} video game playthrough`);
 
     useEffect(() => {
+        getGameShow(gameSlug);
         setTimeout(() => {
             if (!videos) {
                 setQuotaDone(true);
             }
-        }, 4000)
+        }, 4000);
+
+        return () => {
+            resetGameShow();
+        }
     }, []);
 
     useEffect(() => {
         setSelectedVideo(videos[0]);
     }, [videos]);
 
-    const formatSlug = () => {
-        if (gameSlug) {
-            const words = gameSlug.split('-');
-            return words.map(w => w[0].toUpperCase() + w.slice(1)).join(' ');
-        }
-    };
-
     const renderContent = () => {
         //! display loader, youtube content, or no-more-quota page to user
         if (quotaDone) {
-            return <NoVideosPage />
+            return <NoVideosPage gameSlug={gameSlug} gameShow={gameShow} />
         } else if (!quotaDone && !selectedVideo) {
             return <PageLoader />
         } else {
@@ -49,7 +48,7 @@ const YoutubeVideoPlayer = ({ gameSlug }) => {
                     <BackButton label="back to game page" url={`/games/${gameSlug}`} />
                     <Row className="justify-content-center mt-2">
                         <Card style={{borderRadius: '20px'}} className="col-10 col-md-12 p-0">
-                            <h1 className="youtube-header text-center py-4">{formatSlug()} Videos</h1>
+                            <h1 className="youtube-header text-center py-4">{gameShow.name} Videos</h1>
                             <YoutubeSearch
                                 gameSlug={gameSlug.split('-').join(' ')}
                                 onSearchSubmit={search}
@@ -90,4 +89,10 @@ const YoutubeVideoPlayer = ({ gameSlug }) => {
     );
 };
 
-export default YoutubeVideoPlayer;
+const mapStateToProps = state => {
+    return { gameShow: state.gameShow }
+};
+
+const { getGameShow, resetGameShow } = action.games;
+
+export default connect(mapStateToProps, { getGameShow, resetGameShow })(YoutubeVideoPlayer);
