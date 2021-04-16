e2d9935cd8a5c86de118f0fbdb431e7cd5652956
Erik Huang <ekhu94@gmail.com>
2021-04-16 09:25:09 -0700

    add different loader imgs, adjust max-width on discussion posts in forum

--- src/components/PageLoader.js
+++ src/components/PageLoader.js
@@ -2,33 +2,38 @@ import React, { useState, useEffect } from 'react';
 import { Row } from 'react-bootstrap';
 import { Dimmer, Loader, Image } from 'semantic-ui-react';
 import pikachuGif from '../assets/icons/loading-pikachu.gif';
+import marioBrosGif from '../assets/icons/loading-mario-bros.gif';
+import kirbyGif from '../assets/icons/loading-kirby.gif';
 
+const gifs = [pikachuGif, marioBrosGif, kirbyGif];
 
 const PageLoader = () => {
     const [delayed, setDelayed] = useState(false);
+    const [gif, setGif] = useState(null);
 
     useEffect(() => {
         setTimeout(() => {
             setDelayed(true);
         }, 7000);
 
+        const idx = Math.floor(Math.random() * 3);
+        setGif(gifs[idx]);
+
         return () => {
             setDelayed(false);
+            setGif(null);
         }
     }, []);
 
     return (
         <div>
             <Dimmer active style={{marginTop: '65px',height: '100vh', minHeight: '100vh'}}>
-                <Image style={{margin: '0 auto', width: '250px'}} src={pikachuGif} alt="loading..." />
+                {gif ? <Image style={{margin: '0 auto', width: '250px'}} src={gif} alt="loading..." /> : null }
                 <h5 style={{letterSpacing: '0.1rem'}} className="text-center">{delayed ? "Sometimes this takes a little while..." : "Loading...Please wait..."}</h5>
 
                 {/* disabled */}
                 <Loader disabled size='huge'>{delayed ? "Sometimes this takes a while..." : "Loading...Please wait..."}</Loader>
             </Dimmer>
-            {/* <Image src='/images/wireframe/short-paragraph.png' />
-            <Image src='/images/wireframe/short-paragraph.png' />
-            <Image src='/images/wireframe/short-paragraph.png' /> */}
         </div>
     );
 };
