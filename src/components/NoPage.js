9c87ad4358ce1ba21ceb49296ff1c13f3500a8b3
Erik Huang <ekhu94@gmail.com>
2021-04-16 13:50:59 -0700

    very minor punc on NoPage

--- src/components/NoPage.js
+++ src/components/NoPage.js
@@ -4,8 +4,8 @@ import backgroundImg from '../assets/nopage-luigi.jpg';
 const NoPage = () => {
     return (
         <div className="pt-5">
-            <h3 className="text-center my-3 col-12">Ohh, Mamma Mia...404</h3>
-            <h4 className="text-center mb-5 col-12">Something's wrong with the link!</h4>
+            <h3 className="text-center my-3 col-12">Ohh, Mamma Mia...404!</h3>
+            <h4 className="text-center mb-5 col-12">Something's wrong with the link.</h4>
             <div className="row justify-content-center">
             <img className="col-12 col-sm-8 col-md-5" src={backgroundImg} alt="no-page" />
             </div>
